6a10ab5277216053abeece070b9a0f684ff37939
jalya07 <jalavh@code.edu.az>
2026-05-06 22:26:15 +0400

    change

--- ApiLab/Models/Group.cs
+++ ApiLab/Models/Group.cs
@@ -2,5 +2,7 @@ namespace ApiLab.Models;
 
 public class Group
 {
-    
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
 }
--- ApiLab/Models/Student.cs
+++ ApiLab/Models/Student.cs
@@ -2,5 +2,9 @@ namespace ApiLab.Models;
 
 public class Student
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int GroupId { get; set; }
+    public Group Group { get; set; }
 }
